0483cd1f8a3423813299fd2f5beed22e78f42eea
Harri Lehtola <peruukki@hotmail.com>
2015-03-07 20:32:23 +0100

    Remove unused main menu with settings item

--- app/src/main/java/com/simplecurrencyconverter/MainActivity.java
+++ app/src/main/java/com/simplecurrencyconverter/MainActivity.java
@@ -4,8 +4,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -115,27 +113,5 @@ public class MainActivity extends ActionBarActivity {
     private String formatAmount(BigDecimal amount, DecimalFormat formatter) {
         String formattedValue = formatter.format(amount.doubleValue());
         return formattedValue.equals(emptyAmount) ? "" : formattedValue;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
