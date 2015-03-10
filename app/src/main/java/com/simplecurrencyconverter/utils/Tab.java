804e1676bb8ce836a7c7524913210d806ee81554
Harri Lehtola <peruukki@hotmail.com>
2015-03-10 15:21:52 +0100

    Add documentation for the Tab class

--- app/src/main/java/com/simplecurrencyconverter/utils/Tab.java
+++ app/src/main/java/com/simplecurrencyconverter/utils/Tab.java
@@ -7,15 +7,31 @@ public class Tab {
     private Fragment mContent;
     private String mTitle;
 
+    /**
+     * A container for tab title and its content Fragment.
+     *
+     * @param title  the tab title
+     * @param content  the tab content Fragment
+     */
     public Tab(String title, Fragment content) {
         mTitle = title;
         mContent = content;
     }
 
+    /**
+     * Returns the tab content Fragment.
+     *
+     * @return the tab content
+     */
     public Fragment getContent() {
         return mContent;
     }
 
+    /**
+     * Returns the tab title.
+     *
+     * @return the tab title
+     */
     public String getTitle() {
         return mTitle;
     }
